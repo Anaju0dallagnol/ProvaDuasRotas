f4ddb776189ad3f5dc8b2a5e6a3b1c821e098d6d
Anaju0dallagnol <juliana.prado001@gmail.com>
2025-06-09 15:29:44 -0300

    feat-add-Card

---
+++ src/Componentes/Card.tsx
@@ -0,0 +1,44 @@
+import { Card, CardActions, CardContent, CardMedia, Typography } from "@mui/material";
+
+interface ButtonsProps {
+
+    titulo:String
+    textoMeio:String;
+    postID:any;
+    postUserID: any;
+
+  }
+  
+  export default function ComponentButton({titulo,textoMeio,postID,postUserID }: ButtonsProps) {
+    return (
+      <div>
+        
+
+        <Card sx={{ maxWidth: 345 }}>
+
+        <Typography gutterBottom variant="h5" component="div">
+            {titulo}
+          </Typography>
+
+        <CardMedia
+          sx={{ height: 140 }}
+          image="https://img.freepik.com/photos-gratuite/vue-du-cameleon-couleurs-neons-vives_23-2151682767.jpg?semt=ais_hybrid&w=740"
+
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {titulo}
+          </Typography>
+          <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+            {textoMeio}
+          </Typography>
+        </CardContent>
+        <CardActions>
+        <Typography variant="subtitle2">Post ID: {postID}</Typography>
+        <Typography variant="subtitle2">User ID: {postUserID}</Typography>
+        </CardActions>
+      </Card>
+      </div>
+
+    );
+  }
